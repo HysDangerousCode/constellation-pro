ddf0bad904010d0e46255764129c53b2c3c87c81
hys81021 <hys81021@163.com>
2020-11-09 23:57:26 +0800

    指数组件

--- src/views/Today.vue
+++ src/views/Today.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="container">
     <ConsCard :name="todayData.name" :allIndex="todayData.all" />
+    <NumList :data="todayData"/>
   </div>
 </template>
 <script>
 import { onMounted, computed } from "vue";
 import { useStore } from "vuex";
 import getData from "@/services";
+import NumList from "@/components/NumList";
 
 export default {
   name: "TodayPage",
+  components:{
+      NumList
+  },
   setup() {
     const store = useStore(),
       state = store.state;
     onMounted(() => {
       getData(store);
-      console.log(state);
     });
     return {
       todayData: computed(() => state.today),
